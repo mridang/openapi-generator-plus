f371cdf507df08345660df6f69d6f584f8f1c19b
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-07-18 10:03:06 +0530

    chore: add scripts to allow dumping templates

---
+++ src/main/resources/templates/node/dump-templates.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+docker run --rm \
+  --volume=".:/templates" \
+  openapitools/openapi-generator-cli:v7.14.0 author template \
+    --generator-name=node \
+    --output=/templates
